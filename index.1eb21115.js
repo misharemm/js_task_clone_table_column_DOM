03c28f9cefec174047b74c05b48aefec545ed445
Misha Remeslo <misha.remeslol@gmail.com>
2024-07-23 19:04:08 +0300

    make build

---
+++ index.1eb21115.js
@@ -0,0 +1,2 @@
+var e=document.getElementsByTagName("table")[0],l=e.querySelector("thead tr"),r=l.cells[1].cloneNode(!0);l.insertBefore(r,l.cells[4]);var o=e.querySelector("tfoot tr"),t=l.cells[1].cloneNode(!0);o.insertBefore(t,o.cells[4]),e.querySelectorAll("tbody tr").forEach(function(e){var l=e.cells[1].cloneNode(!0);e.insertBefore(l,e.cells[4])});
+//# sourceMappingURL=index.1eb21115.js.map
